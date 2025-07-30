f6288839467bd10a680f20ea078c30723774d757
lakshhkdusv <cookiesnew0@gmail.com>
2025-07-30 17:36:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/lakshhkdusv/Pihu-Music",
+    "https://github.com/lakshhkdusv/Pihunew",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
